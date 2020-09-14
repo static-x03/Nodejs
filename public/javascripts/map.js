874d2259f53e01a0660c1a7eeb39b08b1084bb17
brayan.vanegas <brayan.vanegas@grupomok.com>
2020-09-13 22:46:43 -0500

    agregando map y stylos

---
+++ public/javascripts/map.js
@@ -0,0 +1,5 @@
+var map = L.map('map_id').setView([4.6524,-74.1165], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
